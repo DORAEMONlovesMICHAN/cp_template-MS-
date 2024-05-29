16357dff423520a014da92a43d2680e27b6a58d4
DORAEMONlovesMICHAN <143796666+DORAEMONlovesMICHAN@users.noreply.github.com>
2024-05-29 07:15:11 +0530

    Add files via upload

---
+++ my_template.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*code by Sushanth_iitbbs*/
+/*cf profile :- Sushanth_iitbbs*/
+
+
+// some data types
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+// some for loops
+
+
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define ef(i,s,e) for(long long int i=s;i<=e;i++)
+#define pb push_back
+#define eb emplace_back
+
+
+
+void solve (void)
+{
+
+}
+int main(void)
+{
+    ll tc;
+    cin>>tc;
+    while(tc>0)
+    {
+        solve();
+        tc--;
+    }
+    return 0;
+}
